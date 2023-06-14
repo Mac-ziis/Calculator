e1552ae21379434bca4b31e58fbb768765b9e41c
Mac-ziis <macg286@gmail.com>
2023-06-13 19:24:20 -0700

    Add updated html and script.js files

--- js/script.js
+++ js/script.js
@@ -1,24 +1,42 @@
-function add(number1, number2) {
-  return number1 + number2;
+// Business Logic
+function add(num1, num2) {
+  return num1 + num2;
 }
 
-function subtract(number1, number2) {
-  return number1 - number2;
+function subtract(num1, num2) {
+  return num1 - num2;
 }
 
-function multiply(number1, number2) {
-  return number1 * number2;
+function multiply(num1, num2) {
+  return num1 * num2;
 }
 
-function divide(number1, number2) {
-  return number1 / number2;
+function divide(num1, num2) {
+  return num1 / num2;
 }
 
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
+// User Interface Logic
+function handleCalculation(event) {
+  event.preventDefault();
+  const number1 = parseInt(document.querySelector("input#input1").value);
+  const number2 = parseInt(document.querySelector("input#input2").value);
+  const operator = document.querySelector("input[name='operator']:checked").value;
 
-//window.alert("The additon of your numbers equals: " + add(number1, number2) + "." + " The subtraction of your numbers equals: " + subtract(number1, number2) + "." + " The multiplication of your numbers equals: " + multiply(number1, number2) + "." + " The division of your numbers equals: " + divide(number1, number2) + ".");
+  let result;
+  if (operator === "add") {
+    result = add(number1, number2);
+  } else if (operator === "subtract") {
+    result = subtract(number1, number2);
+  } else if (operator === "multiply") {
+    result = multiply(number1, number2);
+  } else if (operator === "divide") {
+    result = divide(number1, number2);
+  }
 
+  document.getElementById("output").innerText = result;
+}
 
-window.alert((number1) + " + " + (number2) + " = " + add(number1, number2) + ". " + (number1) + " - " + (number2) + " = " + subtract(number1, number2) + ". " + (number1) + " * " + (number2) + " = " + multiply(number1, number2) + ". " + (number1) + " / " + (number2) + " = " + divide(number1, number2) + ".");
-
+window.addEventListener("load", function() {
+  const form = document.getElementById("calculator");
+  form.addEventListener("submit", handleCalculation);
+});
